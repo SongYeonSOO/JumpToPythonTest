14f3c323257701e65187dd3d73e76d5251981fdb
yeonsoo-song <yeonsoo.song@navercorp.com>
2020-07-18 22:41:11 +0900

    int test

---
+++ int_test/int_test.py
@@ -0,0 +1,18 @@
+# octal
+a = 0o177
+# hexDecimal
+b = 0x8ff
+print("a=", a)
+print("b=", b)
+print("a + b=", a + b)
+
+# square of n
+c = 2
+d = 4
+print("c^d=", c**d)
+
+# quotient
+e=7
+f=3
+print("e/f=", e/f)
+print("e//f=", e//f)
